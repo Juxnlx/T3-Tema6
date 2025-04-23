d9c2bd8bfad1bb1720348f4ba6640a3e137f9252
jlbarrionuevo <jlbarrionuevo@106-02.info2>
2025-04-23 08:51:00 +0200

    Ejercicio 6 - Completo

--- Tema6/src/ejercicio6/Ejercicio6.java
+++ Tema6/src/ejercicio6/Ejercicio6.java
@@ -13,16 +13,45 @@ public class Ejercicio6 {
 
 	public static void main(String[] args) {
 
+		// Creamos una lista llamada numerosEnteros donde almacenar una serie de números
+		// que lee de un fichero.
+		ArrayList<Integer> numeros = new ArrayList<Integer>();
+
+		// Almacenamos en nuestra lista la lista que nos devuelve la función Lectura.
+		numeros = lectura();
+
+		// Ordenamos la lista.
+		Collections.sort(numeros);
+
+		// Llamamos a la función escritura para que nos lea la lista pasada como
+		// parametro y escriba los números en otro fichero.
+		escritura(numeros);
+	}
+
+	/**
+	 * Esta función se encarga de leer los números de un fichero y los añade a la
+	 * lista que nos creamos, que sera la que luego devolvamos.
+	 * 
+	 * @return La lista que nos acabamos de crear con los números leidos del
+	 *         fichero.
+	 */
+	public static ArrayList<Integer> lectura() {
+		// Creamos una lista llamada numerosEnteros donde almacenar una serie de números
+		// que lee de un fichero.
 		ArrayList<Integer> numerosEnteros = new ArrayList<Integer>();
 
-		// Creamos la variable num como int para almacenar el número que estamos leyendo
+		// Creamos la variable num como int para almacenar el número que esta leyendo
 		// del fichero.
 		int num;
 
+		// Capturamos el codigo susceptible a lanzar alguna excepción. Capturamos en el
+		// try el Scanner para que se cierre cuando termine de ejecutarse el
+		// bloque try.
 		try (Scanner sc = new Scanner(new FileReader("src\\ejercicio6\\NumerosEntDesor.txt"))) {
 
+			// Comprobamos si existen números enteros en el documento.
 			while (sc.hasNextInt()) {
-				// Leemos el número y lo almacenamos en la variable numero.
+				// Leemos el número y lo almacenamos en la variable num.
 				num = sc.nextInt();
 
 				// Almacenamos en la lista el número que estamos leyendo del fichero.
@@ -34,11 +63,25 @@ public class Ejercicio6 {
 			System.out.println("Error al leer el fichero.");
 		}
 
-		Collections.sort(numerosEnteros);
+		// Devolvemos nuestra lista creada.
+		return numerosEnteros;
+	}
 
+	/**
+	 * Esta función se encarga de recorrer la lista pasada por parametro, ir
+	 * recorriendo cada número de esa lista y los añade a otro fichero.
+	 * 
+	 * @param num La lista con los números a escribir.
+	 */
+	public static void escritura(ArrayList<Integer> num) {
+		// Capturamos el codigo susceptible a lanzar alguna excepción. Capturamos en el
+		// try el BufferedWriter para que se cierre cuando termine de ejecutarse el
+		// bloque try.
 		try (BufferedWriter bw = new BufferedWriter(new FileWriter("src\\ejercicio6\\NumerosEntOrd.txt", true))) {
 
-			for (int numeros : numerosEnteros) {
+			// Recorremos la lista para ir añadiendo cada número a un nuevo fichero.
+			for (int numeros : num) {
+				// Añadimos el número que vamos leyendo.
 				bw.write(String.valueOf(numeros));
 				// Llamamos al metodo del BufferedWriter para añadir un salto de linea.
 				bw.newLine();
@@ -53,5 +96,4 @@ public class Ejercicio6 {
 			System.out.println("Ha habido algún problema al abrir el fichero");
 		}
 	}
-
 }
